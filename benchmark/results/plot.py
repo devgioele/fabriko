ea78afd4558ddc270a8b0c1b776fc55081f27238
devgioele <gioeledevittidev@gmail.com>
2022-09-02 23:16:57 +0200

    Scale axes to increase visibility

--- benchmark/results/plot.py
+++ benchmark/results/plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 def mean(list):
     return sum(durations) / len(durations)
@@ -16,8 +17,16 @@ def median(list):
 def distance(a, b):
     return (a - b) / b * 100
 
-def genFigure(iteration, durationTitle):
+def flattenIteration(iteration):
     durations = []
+    # Collect durations to get minimum
+    for rolesMatrix in iteration:
+        for rasterArray in rolesMatrix:
+            for duration in rasterArray:
+                durations.append(duration)
+    return durations
+
+def genFigure(iteration, durationTitle, durationMin, durationMax):
     figure = plt.figure(figsize=(10.5, 9))
     figure.tight_layout() # pad=0, w_pad=0, h_pad=0
     subplots = [
@@ -25,6 +34,7 @@ def genFigure(iteration, durationTitle):
         figure.add_subplot(2, 2, 2, projection='3d'),
         figure.add_subplot(2, 2, (3, 4), projection='3d')
     ]
+    durationOffset = 15
     # Generate a plot for each role
     for index, rolesMatrix in enumerate(iteration):
         roles = index + 1
@@ -42,10 +52,9 @@ def genFigure(iteration, durationTitle):
         dz = []
         for rasterArray in rolesMatrix:
             for w in rasterArray:
-                dz.append(w)
-        durations.extend(dz)
+                dz.append(w-durationMin+durationOffset)
         # Let each bar touch the ground
-        z = np.zeros_like(x)
+        z = np.repeat(durationMin-durationOffset, len(x))
         # Make each bar smaller such that there is space in between
         dx = dy = 1/2 * np.ones_like(z)
         # Shift each bar such that it is centered
@@ -59,13 +68,17 @@ def genFigure(iteration, durationTitle):
         # depth of each bar
         # height of each bar
         subplot.bar3d(x, y, z, dx, dy, dz, shade=True, color='#008233')
-        subplot.set_zlim((0, 400))
-        subplot.view_init(15, -66)
+        subplot.set_zlim(zmin=durationMin-durationOffset, zmax=durationMax)
+        # subplot.view_init(15, -66)
+        subplot.view_init(14, -81)
         subplot.set_title('Roles: ' + str(roles), y=0.97)
         subplot.set_xlabel('Raster files')
         subplot.set_ylabel('Vector files')
         subplot.set_zlabel(durationTitle)
-    return figure, durations
+        newLines = durationTitle.count("\n")
+        labelpad = 15 * newLines if newLines > 0 else 10
+        subplot.zaxis.labelpad = labelpad
+    return figure
 
 # DATA
 # x = number raster files
@@ -134,35 +147,29 @@ data = [
 ]]]
 
 # Generate a figure that takes the mean of all iterations
-meanIteration = [[
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-    ],
-    [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-    ],
-    [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-    ]]
+meanIteration = np.zeros_like(data[0])
 for i in range(0, len(data)):
     iteration = data[i]
     for indexRoles, rolesMatrix in enumerate(iteration):
         for indexRaster, rasterArray in enumerate(rolesMatrix):
             for indexVector, duration in enumerate(rasterArray):
                 meanIteration[indexRoles][indexRaster][indexVector] += duration/len(data)
-fig, _ = genFigure(meanIteration, 'Arithmetic mean of duration [s]')
+durationsMean = flattenIteration(meanIteration)
+durationMinMean = min(durationsMean)
+durationMaxMean = max(durationsMean)
+fig = genFigure(meanIteration, 'Arithmetic mean\nof duration [s]', durationMinMean, durationMaxMean)
 fig.savefig(fname="benchmark.png", dpi=400)
 
-# Generate a figure for each benchmark iteration
+# Collect durations and compute min and max
 durations = []
+for iteration in data:
+    durations.extend(flattenIteration(iteration))
+durationMin = min(durations)
+durationMax = max(durations)
+
+# Generate a figure for each benchmark iteration
 for index, iteration in enumerate(data):
-    fig, d = genFigure(iteration, 'Duration [s]')
-    durations.extend(d)
+    fig = genFigure(iteration, 'Duration [s]', durationMin, durationMax)
     # Save figure
     fig.savefig(fname="benchmark-iteration-" + str(index+1) + ".png", dpi=400)
 
@@ -172,9 +179,7 @@ def roundStats(v):
 print("-- Statistics about duration --")
 mean = mean(durations)
 print("Arithmetic mean = {}s".format(roundStats(mean)))
-minimum = min(durations)
-print("Min = {}s = {:+}%".format(roundStats(minimum), roundStats(distance(minimum, mean))))
-maximum = max(durations)
-print("Max = {}s = {:+}%".format(roundStats(maximum), roundStats(distance(maximum, mean))))
+print("Min = {}s = {:+}%".format(roundStats(durationMin), roundStats(distance(durationMin, mean))))
+print("Max = {}s = {:+}%".format(roundStats(durationMax), roundStats(distance(durationMax, mean))))
 
 plt.show()
